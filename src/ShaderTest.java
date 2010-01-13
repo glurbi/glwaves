92e0848c9a476c940b5655a0a0a1802758f9b8ab
Vincent Gay <glurbi@gmail.com>
2010-01-13 15:38:29 +0100

    six

--- src/ShaderTest.java
+++ src/ShaderTest.java
@@ -4,7 +4,6 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -18,14 +17,21 @@ import com.sun.opengl.util.BufferUtil;
 
 public class ShaderTest {
 
-    private static int points = 1000;
-    private static int lines = 100;
+    private static final int points = 1000;
+    private static final int lines = 100;
+    private static final float mouseAccel = 0.01f;
 
-    private volatile static float zoomFactor = 1.0f;
+    private volatile static float scale = 1.0f;
+    private volatile static float xTranslate = 0.0f;
+    private volatile static float yTranslate = 0.0f;
+    private volatile static float xRotate = 0.0f;
+    private volatile static float yRotate = 0.0f;
     
     private static FloatBuffer vertices = createVertices();
     private static FloatBuffer colors = createColors();
 
+    private static AtomicLong frameCount = new AtomicLong();
+    
     private static FloatBuffer createVertices() {
         FloatBuffer buf = BufferUtil.newFloatBuffer(points*lines*2*4*4);
         for (int line = 0; line < lines; line++) {
@@ -56,55 +62,42 @@ public class ShaderTest {
         return buf;
     }
 
-    public static void main(String[] args) {
-        final long t0 = System.currentTimeMillis();
-        final AtomicLong frameCount = new AtomicLong();
-        Frame frame = new Frame();
-        frame.addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
+    private static class MyMouseListener extends MouseAdapter {
+        
+        private int lastX;
+        private int lastY;
+        
+        public void mouseWheelMoved(MouseWheelEvent e) {
+            int rotation = e.getWheelRotation();
+            if (rotation > 0) {
+                scale /= rotation * 2;
+            } else if (rotation < 0) {
+                scale *= -rotation * 2;
             }
-        });
-        final GLCanvas canvas = new GLCanvas();
-        final GLEventListener myEventListener = new MyEventListener();
-        canvas.addGLEventListener(myEventListener);
-        frame.add(canvas);
-        frame.setSize(300, 300);
-        frame.setVisible(true);
-        MouseAdapter myMouseAdapter = new MouseAdapter() {
-            public void mouseWheelMoved(MouseWheelEvent e) {
-                int rotation = e.getWheelRotation();
-                if (rotation > 0) {
-                    zoomFactor *= rotation * 2;
-                } else if (rotation < 0) {
-                    zoomFactor /= -rotation * 2;
-                }
-                System.out.println("zoomFactor=" + zoomFactor);
-            }
+            System.out.println("zoomFactor=" + scale);
+        }
 
-            public void mouseDragged(MouseEvent e) {
+        public void mousePressed(MouseEvent e) {
+            lastX = e.getX();
+            lastY = e.getY();
+        }
+        
+        public void mouseDragged(MouseEvent e) {
+            int difX = e.getX() - lastX;
+            int difY = e.getY() - lastY;
+            lastX = e.getX();
+            lastY = e.getY();
+            if ((e.getModifiers() & MouseEvent.BUTTON1_MASK) != 0) {
+                xTranslate += mouseAccel * difX / scale;
+                yTranslate -= mouseAccel * difY / scale;
+            } else if ((e.getModifiers() & MouseEvent.BUTTON3_MASK) != 0) {
+                xRotate += difY;
+                yRotate += difX;
             }
-        };
-        frame.addMouseWheelListener(myMouseAdapter);
-        new Thread(new Runnable() {
-            public void run() {
-                while (true) {
-                    canvas.display();
-                    frameCount.incrementAndGet();
-                    Thread.yield();
-                }
-            }
-        }).start();
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-            public void run() {
-                long totalTime = System.currentTimeMillis() - t0;
-                System.out.println(totalTime + " ms");
-                System.out.println(frameCount.get() + " frames");
-                System.out.println(1.0 * frameCount.get() / totalTime * 1000);
-            }
-        });
-    }
-
+        }
+        
+    };
+    
     public static class MyEventListener implements GLEventListener {
 
         public void init(GLAutoDrawable drawable) {
@@ -120,14 +113,18 @@ public class ShaderTest {
 
             gl2.glMatrixMode(GL2.GL_PROJECTION);
             gl2.glLoadIdentity();
-            gl2.glFrustum(-0.5f * zoomFactor, 0.5f * zoomFactor, -0.5f * zoomFactor / 2, 0.5f * zoomFactor / 2, 1.0f,
-                    10.0f);
+            gl2.glFrustum(-0.5f, 0.5f, -0.5, 0.5f, 1.0f, 100.0f);
             
             gl2.glMatrixMode(GL2.GL_MODELVIEW);
             gl2.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+            
             gl2.glLoadIdentity();
-
-            gl2.glTranslatef(-1.5f, -0.0f, -6.0f);
+            gl2.glTranslatef(xTranslate, yTranslate, 0.0f);
+            gl2.glTranslatef(-1.5f, -0.0f, -5.0f);
+            gl2.glRotatef(xRotate, 1, 0, 0);
+            gl2.glRotatef(yRotate, 0, 1, 0);
+            gl2.glScalef(scale, scale, scale);
+            
             gl2.glBegin(GL.GL_TRIANGLES);
             gl2.glColor3f(1.0f, 1.0f, 1.0f);
             gl2.glVertex3f(0.0f, 1.0f, 0.0f);
@@ -160,6 +157,44 @@ public class ShaderTest {
         public void dispose(GLAutoDrawable drawable) {
         }
 
+    }
+
+    
+    public static void main(String[] args) {
+        final long t0 = System.currentTimeMillis();
+        Frame frame = new Frame();
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        final GLCanvas canvas = new GLCanvas();
+        final GLEventListener myEventListener = new MyEventListener();
+        MyMouseListener myMouseListener = new MyMouseListener();
+        canvas.addGLEventListener(myEventListener);
+        canvas.addMouseListener(myMouseListener);
+        canvas.addMouseWheelListener(myMouseListener);
+        canvas.addMouseMotionListener(myMouseListener);
+        frame.add(canvas);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+        new Thread(new Runnable() {
+            public void run() {
+                while (true) {
+                    canvas.display();
+                    frameCount.incrementAndGet();
+                    Thread.yield();
+                }
+            }
+        }).start();
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                long totalTime = System.currentTimeMillis() - t0;
+                System.out.println(totalTime + " ms");
+                System.out.println(frameCount.get() + " frames");
+                System.out.println(1.0 * frameCount.get() / totalTime * 1000);
+            }
+        });
     }
 
 }
